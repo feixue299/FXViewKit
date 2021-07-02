9311d49a7314fa2b9c62552700d2031db53b7e3c
wupengfei <ariablink299@gmail.com>
2021-07-02 16:24:09 +0800

    Update AppTheme.swift

--- Sources/Tools/AppTheme.swift
+++ Sources/Tools/AppTheme.swift
@@ -17,20 +17,21 @@ public struct AppTheme {
     public let interaction: UIColor
     public let interaction1: UIColor
     public let label: UIColor
+    
     public let label1: UIColor
     
     public static var currentTheme: AppTheme = dark
     public static let dark = AppTheme(
-        activity: UIColor(hexString: "388FCB")!,
-        activity1: UIColor(hexString: "00C4EA")!,
-        background: UIColor(hexString: "0F0F0F")!,
-        background1: UIColor(hexString: "17171A")!,
-        gradient_end: UIColor(hexString: "EB00D7")!,
-        gradient_start: UIColor(hexString: "0996E6")!,
-        interaction: UIColor(hexString: "97139F")!,
-        interaction1: UIColor(hexString: "611797")!,
-        label: UIColor(hexString: "FFFFFF")!,
-        label1: UIColor(hexString: "545455")!)
+        activity: #colorLiteral(red: 0.2196078431, green: 0.5607843137, blue: 0.7960784314, alpha: 1),
+        activity1: #colorLiteral(red: 0, green: 0.768627451, blue: 0.9176470588, alpha: 1),
+        background: #colorLiteral(red: 0.05882352941, green: 0.05882352941, blue: 0.05882352941, alpha: 1),
+        background1: #colorLiteral(red: 0.09019607843, green: 0.09019607843, blue: 0.1019607843, alpha: 1),
+        gradient_end: #colorLiteral(red: 0.9215686275, green: 0, blue: 0.8431372549, alpha: 1),
+        gradient_start: #colorLiteral(red: 0.03529411765, green: 0.5882352941, blue: 0.9019607843, alpha: 1),
+        interaction: #colorLiteral(red: 0.5921568627, green: 0.07450980392, blue: 0.6235294118, alpha: 1),
+        interaction1: #colorLiteral(red: 0.3803921569, green: 0.09019607843, blue: 0.5921568627, alpha: 1),
+        label: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1),
+        label1: #colorLiteral(red: 0.3294117647, green: 0.3294117647, blue: 0.3333333333, alpha: 1))
     
     public static var activity: UIColor { return currentTheme.activity}
     public static var activity1: UIColor { return currentTheme.activity1}
